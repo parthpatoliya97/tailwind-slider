6fb71b4effedb6ffef9f11bdce5e25d384d3b00b
parth patoliya <parthpatoliya001@gmail.com>
2025-02-21 23:03:56 +0530

    Initial commit

---
+++ myapp/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,svelte,ts}"],
+    theme: {
+      extend: {
+        animation: {
+          "loop-scroll": "loop-scroll 50s linear infinite",
+        },
+        keyframes: {
+          "loop-scroll": {
+            from: { transform: "translateX(0)" },
+            to: { transform: "translateX(-100%)" },
+          },
+        },
+      },
+    },
+    plugins: [],
+  };
+  
